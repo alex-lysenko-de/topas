72af00c6a2a3b5fb081a3c03d51861f2ba7512b1
alex-lysenko-de <alex.lysenko.de@gmail.com>
2025-08-09 21:03:52 +0200

    version 1.0

--- install/build_all.cmd
+++ install/build_all.cmd
@@ -3,5 +3,7 @@ pause
 call py2exe.cmd
 pause
 copy setup_tesseract.cmd .\dist\setup_tesseract.cmd
-start topas.iss
+start topas.iss /wait
+pause
+powershell -Command "Get-FileHash .\Output\Topas_setup_v1.0.exe -Algorithm SHA256 > Topas_setup_v1.0.sha256.txt"
 
